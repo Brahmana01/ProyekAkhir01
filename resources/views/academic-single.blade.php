40931d4c5278ca6b3bbc5158d61904459e2ced6c
Brahmana01 <sigoelangbatoe@gmail.com>
2025-04-25 16:31:28 +0700

    Initial commit

---
+++ resources/views/academic-single.blade.php
@@ -0,0 +1,120 @@
+@include('layouts.main')
+
+
+
+    <main class="main">
+
+        <!-- breadcrumb -->
+        <div class="site-breadcrumb" style="background: url(assets/img/breadcrumb/01.jpg)">
+            <div class="container">
+                <h2 class="breadcrumb-title">Sejarah Del/D3 Teknologi Informasi</h2>
+                <ul class="breadcrumb-menu">
+                    <li><a href="index.html">Home</a></li>
+                    <li class="active">Sejarah</li>
+                </ul>
+            </div>
+        </div>
+        <!-- breadcrumb end -->
+
+
+        <!-- department-single -->
+        <div class="department-single-area py-120">
+            <div class="container">
+                <div class="department-single-wrapper">
+                    <div class="row">
+                        <div class="col-xl-4 col-lg-4">
+                            <div class="department-sidebar">
+
+                                <div class="widget department-download">
+                                    <h4 class="widget-title">link</h4>
+                                    <a href="http://www.del.ac.id/wp-content/uploads/2014/07/Brosur_D3TI_ITDEL_2425.pdf"><i class="far fa-file-pdf"></i> Brosur D3_TI_2024</a>
+                                    <a href="https://cis.del.ac.id/"><i class="far fa-link"></i> Sistem Informasi Akademik</a>
+                                    <a href="https://sipp.del.ac.id/"><i class="far fa-link"></i> Sistem Informasi Perpustakaan</a>
+                                    <a href="https://www.del.ac.id/"><i class="far fa-link"></i> web resmi IT del</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-xl-8 col-lg-8">
+                            <div class="department-details">
+                                <div class="department-details-img mb-30">
+                                    <img src="assets/img/department/single.jpg" alt="thumb">
+                                </div>
+                                <div class="department-details">
+                                    <h3 class="mb-20"><strong>Sejarah Singkat Del</strong></h3>
+                                    <h4 class="mb-20">Yayasan Del</h4>
+                                    <p class="mb-20">
+                                        Jenderal TNI (Purn.) Luhut B. Pandjaitan mendirikan Yayasan Simargala pada tanggal 30 Agustus 2001 
+                                        di Jakarta. Simargala adalah nama desa kelahiran beliau di Huta Namora, Kecamatan Silaen, Kabupaten Toba Samosir,
+                                         Sumatera Utara. Pendirian Yayasan Simargala dilandasi oleh keinginan luhur untuk meningkatkan keterampilan 
+                                         dan kesejahteraan masyarakat perdesaan yang kurang tersentuh pembangunan. Tekad yang kuat untuk
+                                          berpartisipasi pada program pemerintahan dalam bidang pendidikan, sosial, kemanusiaan,
+                                           seni dan budaya, dan kelestarian lingkungan.
+                                           Masih pada tahun 2001, karena pertimbangan nama yang bersifat lokal,
+                                            Yayasan Simargala diubah menjadi Yayasan Del. Kata “Del” berasal dari 
+                                            usulan salah satu anggota Yayasan untuk menggunakan istilah “Del”, yang 
+                                            mengandung arti “pemimpin yang selalu berada selangkah lebih maju”. Yayasan
+                                             Del adalah organisasi nir laba yang didirikan untuk membawa perubahan dan
+                                              pembaharuan bagi individu dan juga masyarakat.
+                                    </p><br>
+
+                                    </p>
+                                    <div class="my-4">
+                                        <div class="mb-3">
+                                            <h3 class="mb-3"><strong>Sejarah Diploma 3 Teknologi Informasi</strong></h3>
+                                            <div class="row">
+                                                <div class="col-md-6 mb-20">
+                                                    <img src="assets/img/department/01.jpg" alt="">
+                                                </div>
+                                                <div class="col-md-6 mb-20">
+                                                    <img src="assets/img/department/02.jpg" alt="">
+                                                </div>
+                                            </div>
+                                            <p>Program Studi Teknologi Informasi Diploma Tiga  adalah salah satu dari 3 (tiga)
+                                                 program studi yang dikelola oleh Institut Teknologi Del (IT Del) yang berdiri 
+                                                 pada tahun 2001 sesuai SK No. 222/D/O/2001 tertanggal 28 September 2001 dengan
+                                                  nama Program Studi Teknologi Informasi. Program studi ini berlokasi
+                                                   di Jl. Sisingamangaraja, Desa Sitoluama, Kecamatan Laguboti, Toba Samosir, 
+                                                   Propinsi Sumatera Utara berjarak kurang lebih 200 km (lima jam perjalanan mobil)
+                                                    dari Medan sebagai Ibukota Propinsi Sumatera Utara. Desa Sitoluama adalah 
+                                                    suatu desa kecil yang berada di tepi Danau Toba dan dilalui oleh jalan 
+                                                    raya lintas propinsi dan berjarak sekitar 10 KM dari Balige sebagai ibukota Kabupaten Tobasa.</p>
+                                                    <p>Quisque a nisl id sem sollicitudin volutpat. Cras et commodo quam, vel congue
+                                                        ligula. Orci varius natoque penatibus et magnis dis parturient montes,
+                                                        nascetur ridiculus mus. Cras quis venenatis neque. Donec volutpat tellus
+                                                        lobortis mi ornare eleifend. Fusce eu nisl ut diam ultricies accumsan.
+                                                        Integer lobortis vestibulum nunc id porta. Curabitur aliquam arcu sed ex
+                                                        dictum, a facilisis urna porttitor. Fusce et mattis nisl. Sed iaculis libero
+                                                        consequat justo auctor iaculis. Vestibulum sed ex et magna tristique
+                                                        bibendum. Sed hendrerit neque nec est suscipit, id faucibus dolor convallis.
+                                                    </p>
+                                        </div>
+
+                                    </div>
+                                    <div class="my-4">
+                                        <h3 class="mb-3">Kompetensi</h3>
+                                        <ul class="department-single-list">
+                                            <p>Kompetensi lulusan Program Studi Teknologi Informasi Diploma Tiga  yaitu:</p>
+                                            <li><i class="far fa-check"></i>Mampu membangun, mengembangkan dan memelihara perangkat
+                                                 lunak dengan mengikuti dan menerapkan metode rekayasa perangkat 
+                                                 lunak yang baik dan benar, yang mencakup perangkat lunak pada 
+                                                 platform desktop, web, dan mobile.</li>
+                                            <li><i class="far fa-check"></i>Selain mampu dalam bidangnya, juga mempunyai landasan
+                                                 yang cukup terhadap semua bidang dasar informatika: jaringan komputer, 
+                                                 sistem informasi, dan teknologi informasi.</li>
+                                            <li><i class="far fa-check"></i>Mampu untuk bekerja dalam tim.
+                                            </li>
+                                            <li><i class="far fa-check"></i>Mempunyai softskill yang sesuai yaitu disiplin dan 
+                                                sikap kerja yang profesional</li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- department-single end-->
+
+    </main>
+    @include('layouts.footer')
